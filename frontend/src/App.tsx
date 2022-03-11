098c44a9b9fc60d4ee00206d044004e5a2215d80
Carlos Cavalcante <carlosacfilho1992@gmail.com>
2022-03-11 09:08:17 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import Navbar from "./components/Navbar";
+
 
 
 function App() {
-  return (
-   <h1>Página principal</h1>
-  );
+    return (
+        <Navbar />
+    );
 }
 
 export default App;
